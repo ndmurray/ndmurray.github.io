f6e53ac8b47e54af2c162fb212af0cc6607ac2d1
Nick Murray <Nick_Murray@abtassoc.com>
2016-07-04 18:01:05 -0400

    hover for sidebar

--- dist/js/scatter.js
+++ dist/js/scatter.js
@@ -1,8 +1,8 @@
 "use strict";
 var margin = {
-  top: 10,
-  right: 10,
-  bottom: 20,
+  top: 40,
+  right: 140,
+  bottom: 40,
   left: 40
 },
     w = parseInt(d3.select('#scatter-div').style('width'), 10),
@@ -21,6 +21,10 @@ var textShift = 0,
     yaxisShiftX = 0,
     xaxisShiftY = h,
     yaxisShiftY = 0;
+var infoTop = 115,
+    infoLeft = w + margin.left,
+    infoWidth = 12 + "em",
+    infoHeight = 30 + "em";
 var titleText = d3.select("h2#title").append("text.title-text").text("Scatter!");
 d3.csv("datadev/world.csv", function(error, data) {
   if (error) {
@@ -33,33 +37,42 @@ d3.csv("datadev/world.csv", function(error, data) {
     return d.country;
   };
   var dataX = function(d) {
-    return +d.gini;
+    return 100 - +d.press;
   };
   var dataY = function(d) {
-    return +d.press;
+    return +d.corruption;
   };
   var dataR = function(d) {
     return +d.gdphead;
   };
-  var dotTips = d3.tip().attr({class: "d3-tip"}).offset([0, 0]).direction('e').html(function(d) {
-    return "<p id='tiphead'>" + d.country + "</p><p id='tipbody'>GDP/capita: $" + d3.format(',')(+d.gdphead) + "<br />" + +d.gdphead_year + "</p>";
+  var dotTips = d3.tip().attr({class: "d3-tip"}).style({
+    top: infoTop,
+    left: infoLeft,
+    width: infoWidth,
+    height: infoHeight
+  }).direction('e').html(function(d) {
+    return "<p id='tiphead'>" + d.country + "</p><p id='tipbody'><p class='tip-subhead'>Income Group:</p> " + (d.ig) + "</p>" + "</p><p id='tipbody'><p class='tip-subhead'>Region:</p> " + (d.region) + "</p>";
   });
   var svg = d3.select("#scatter-div").append("svg").attr({
     width: w + margin.left + margin.right,
     height: h + margin.top + margin.bottom,
     id: "canvas"
-  }).append("g").attr({transform: "translate(" + margin.left + "," + margin.top + ")"}).call(dotTips);
-  var xScale = d3.scale.linear().domain([0, d3.max(worldData, function(d) {
+  }).append("g").attr({transform: "translate(" + margin.left + "," + margin.top + ")"});
+  var xScale = d3.scale.linear().domain([d3.min(worldData, function(d) {
+    return dataX(d);
+  }), d3.max(worldData, function(d) {
     return dataX(d);
   })]).range([0, w]).nice();
   var yScale = d3.scale.linear().domain([d3.max(worldData, function(d) {
     return dataY(d);
-  }), 0]).range([0, h]);
+  }), d3.min(worldData, function(d) {
+    return dataY(d);
+  })]).range([0, h]);
   var rScale = d3.scale.linear().domain([0, d3.max(worldData, function(d) {
     return dataR(d);
-  })]).range([4, 24]).nice();
-  var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
-  var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+  })]).range([4, 40]).nice();
+  var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+  var yAxis = d3.svg.axis().scale(yScale).orient("left");
   var dots = svg.append("g").attr({id: "dots-group"}).selectAll("circle").data(worldData, key).enter().append("circle").filter(function(d) {
     return dataX(d);
   }).filter(function(d) {
@@ -77,8 +90,28 @@ d3.csv("datadev/world.csv", function(error, data) {
       return rScale(dataR(d));
     },
     class: "dots",
-    "fill": "rgb(179,120,211)"
-  }).on('mouseover', dotTips.show).on('mouseout', dotTips.hide);
+    "pointer-events": "all",
+    "fill": function(d) {
+      if (d.region == "Europe & Central Asia") {
+        return "#CEE879";
+      } else if (d.region == "Middle East & North Africa") {
+        return "#FFA700";
+      } else if (d.region == "Sub-Saharan Africa") {
+        return "#54EBBA";
+      } else if (d.region == "North America") {
+        return "#1DC28C";
+      } else if (d.region == "South Asia") {
+        return "#FF93D2";
+      } else if (d.region == "East Asia & Pacific") {
+        return "#8CD19D";
+      } else if (d.region == "Latin America & Caribbean") {
+        return "#FF0D00";
+      } else {
+        return "black";
+      }
+    }
+  }).call(dotTips).on('mouseenter', dotTips.show).on('mouseleave', dotTips.hide);
+  svg.select("line.xline").append("line").attr("y1", d3.selectAll("circle.dots").attr("cy")).attr("y2", d3.selectAll("circle.dots").attr("cy")).attr("x1", margin.left).attr("x2", d3.selectAll("circle.dots").attr("cx")).attr("class", "xline").attr("stroke", "white").attr("stroke-width", 10);
   svg.append("g").attr({
     class: "xaxis",
     transform: "translate(" + xaxisShiftX + "," + xaxisShiftY + ")"
--- js/scatter.js
+++ js/scatter.js
@@ -1,7 +1,7 @@
 //General use variables
 	
 	//Canvas margin, height, and width by Bostock's margin convention http://bl.ocks.org/mbostock/3019563
-	var	margin = {top: 10, right: 10, bottom: 20, left: 40},
+	var	margin = {top: 40, right: 140, bottom: 40, left: 40},
 		w = parseInt(d3.select('#scatter-div').style('width'), 10),//Get width of containing div for responsiveness
 		w = w - margin.left - margin.right,
 		h = parseInt(d3.select('#scatter-div').style('height'),10),
@@ -10,14 +10,19 @@
 	//Deprecated, margin works instead - Padding between output range and edge of canvas
 	var canvasPadding = {top: 10, right: 10, bottom: 10, left:60};
 		
-	//Default positioning
+	//Default positioning of chart elements
 	var textShift = 0,
 	    dotsShift = 0,
 	    xaxisShiftX = 0,
 	    yaxisShiftX = 0,
 	    xaxisShiftY = h,
 	    yaxisShiftY = 0;
-	    //Using 4 as this is the minimum radius of the dots
+
+	//Positioning of hover information
+	var infoTop = 115,
+		infoLeft = w + margin.left,
+		infoWidth = 12 + "em",
+		infoHeight = 30 + "em";
 
 	//Transitions
 	// var maxDelay = 10000,
@@ -30,7 +35,7 @@
 
 //Text for title 
 	var titleText = d3.select("h2#title").append("text.title-text")
-		.text("Scatter!")
+		.text("Scatter!");
 
 
 //Begin data function
@@ -53,18 +58,21 @@ d3.csv("datadev/world.csv",function(error,data) {
 	};
 
 	//Variables for cx, cy, and r data fields
-	var dataX = function(d) { return +d.gini; };
-	var dataY = function(d) { return +d.press; };
+	var dataX = function(d) { return 100 - +d.press; };
+	//var dataX = function(d) { return 1 - +d.gini/100; };
+	var dataY = function(d) { return +d.corruption; };
+	//var dataY = function(d) { return 100 - +d.press; };
 	var dataR = function(d) { return +d.gdphead; };
 
 //Tooltips - http://bit.ly/22HClnd
 	var dotTips = d3.tip()
 		.attr({class: "d3-tip"})
-		.offset([0, 0])
+		.style({top: infoTop, left: infoLeft, width: infoWidth, height: infoHeight})
+		//size and positioning values in .style not .attr bc tooltip is a div, not svg.
 		.direction('e')
   		.html(function(d) {
-  		  return "<p id='tiphead'>" + d.country + "</p><p id='tipbody'>GDP/capita: $" + d3.format(',')(+d.gdphead) + 
-  		 "<br />" + +d.gdphead_year  + "</p>";
+  		  return "<p id='tiphead'>" + d.country  + "</p><p id='tipbody'><p class='tip-subhead'>Income Group:</p> " + (d.ig) + "</p>"
+  		   + "</p><p id='tipbody'><p class='tip-subhead'>Region:</p> " + (d.region) + "</p>";
  		 });
 
 
@@ -82,35 +90,35 @@ d3.csv("datadev/world.csv",function(error,data) {
 		.append("g") //This g element and it attributes also following bstok's margin convention. It holds all the canvas' elements.
 		.attr({
 			transform: "translate(" + margin.left + "," + margin.top + ")"
-		})
-		.call(dotTips);  //must be called on canvas -  http://bit.ly/22HClnd
+		});
 
 //Define linear scales
 
 	//X scale
 	var xScale = d3.scale.linear()
-		.domain([0, d3.max(worldData,function(d) { return dataX(d); })])
+		.domain([d3.min(worldData,function(d) { return dataX(d); }), d3.max(worldData,function(d) { return dataX(d); })])
+		//Using min as min values in our data in some cases are negative
 		.range([0, w])
 		.nice();
 
 	//Y scale
 	var yScale = d3.scale.linear()
-		.domain([d3.max(worldData,function(d) { return dataY(d); }),0])
+		.domain([d3.max(worldData,function(d) { return dataY(d); }),d3.min(worldData,function(d) { return dataY(d); })])
 		.range([0, h]);
 
 	//Radius scale
 	var rScale = d3.scale.linear()
 		.domain([0, d3.max(worldData,function(d) { return dataR(d); })])
-		.range([4, 24])
+		.range([4, 40])
 		.nice();
 
 //Define axes
 
 	//X axis
-	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
+	var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
 	//Y axis
-	var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
+	var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
 //Default chart elements
 
@@ -131,10 +139,43 @@ d3.csv("datadev/world.csv",function(error,data) {
 				cy: function(d) { return yScale(dataY(d)); },
 				r: function(d) { return rScale(dataR(d)); },
 				class: "dots",
-				"fill": "rgb(179,120,211)"
+				"pointer-events": "all",
+				"fill": function(d) {
+					//colors comaination of the following third party palletes
+					//http://www.colourlovers.com/palette/360922/u.make.me.happy
+					//http://www.colourlovers.com/palette/1689724/%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F					
+					if (d.region == "Europe & Central Asia") { return "#CEE879"; } //yelllow green
+					else if (d. region == "Middle East & North Africa") { return "#FFA700"; }// yellow
+					else if (d. region == "Sub-Saharan Africa") { return "#54EBBA"; } //light teal
+					else if (d.region == "North America") { return "#1DC28C"; } //blue
+					else if (d.region == "South Asia") { return "#FF93D2"; } //light pink
+					else if (d.region == "East Asia & Pacific") { return "#8CD19D"; } //sea foam
+					else if (d.region == "Latin America & Caribbean") { return "#FF0D00"; } //red
+					else { return "black"; }
+				}
 			})
-			.on('mouseover',dotTips.show)
-			.on('mouseout',dotTips.hide);
+					.call(dotTips) //must be called on canvas -  http://bit.ly/22HClnd
+
+			//using enter and leave as opposed to over and out because mouseenter and mouesleave don't bubble up to the dots group 
+			.on('mouseenter',dotTips.show)
+			// .on('mouseenter',function() {
+			// 	d3.select(this).classed("active-dot",true)
+			// 	// dotTips.show;
+			// })
+			.on('mouseleave',dotTips.hide);
+	
+	//Lines connecting dots to axes
+	svg.select("line.xline")
+		.append("line")
+		.attr("y1",d3.selectAll("circle.dots").attr("cy"))
+		.attr("y2",d3.selectAll("circle.dots").attr("cy"))
+		.attr("x1",margin.left)
+		.attr("x2",d3.selectAll("circle.dots").attr("cx"))
+		.attr("class","xline")
+		.attr("stroke", "white")
+		.attr("stroke-width", 10);
+			
+
 
 
 //Call axes
